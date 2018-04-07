3e541fd2a35b024990a570e492a825f61baed891
fitharwe <jesterenwalker@gmail.com>
2018-04-06 22:23:51 -0400

    javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+var yourScore = document.getElementById('yourScore');
+var losses = document.getElementById('losses');
+yourScore.innerHTML = 0; //
+var targetNumber = document.getElementById('targetNumber');
+targetNumber.innerHTML = getRandomValue(10, 4);
+
+function addToScore(val) {
+  var numberToAdd = parseInt(val);
+  var currentScore = parseInt(yourScore.innerHTML);
+  yourScore.innerHTML = numberToAdd + currentScore;
+}
+
+var gem2 = document.getElementById("gem2");
+//gem2.setAttribtarute("value", "5");
+console.log(gem2.value);
+
+var gem1 = document.getElementById("gem1");
+
+
+var gem3 = document.getElementById("gem3");
+
+
+var gem4 = document.getElementById("gem4");
+
+gem2.addEventListener('click', function() {
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+gem1.addEventListener('click', function() {
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+gem3.addEventListener('click', function() {
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+gem4.addEventListener('click', function() {
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+function getRandomValue (a, b){
+  return Math.floor((Math.random() * a) + b);
+}
+
+function checkPlayerScore() {
+  var playerInt = parseInt(yourScore.innerHTML);
+  var targetInt = parseInt(targetNumber.innerHTML);
+  if (playerInt === targetInt) {
+      alert('You win!');
+  } else if (playerInt > targetInt) {
+    alert('You lose!');
+  } 
+}
+
+console.log(getRandomValue(10, 4));
