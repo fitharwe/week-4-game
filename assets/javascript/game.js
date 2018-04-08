5e69a9009e22af4fdf3ba60203c6f83bf718426c
fitharwe <jesterenwalker@gmail.com>
2018-04-08 18:31:24 -0400

    more style changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var yourScore = document.getElementById('yourScore');
-var losses = document.getElementById('losses');
+var loser = document.getElementById('losses');
+var winner = document.getElementById('wins');
 yourScore.innerHTML = 0; //
 var targetNumber = document.getElementById('targetNumber');
 targetNumber.innerHTML = getRandomValue(10, 4);
@@ -57,3 +58,17 @@ function checkPlayerScore() {
 }
 
 console.log(getRandomValue(10, 4));
+
+tempString = loser + "";
+for (var i = tempString.length; i < 43; i++) {
+  tempString += " ";
+}
+totalLoser.textContent = "Losses " + tempString;
+
+tempString = winner + "";
+	for (var i = tempString.length; i < 45; i++) {
+		tempString += " ";
+	}
+  totalWinner.textContent = "Wins " + tempString;
+  
+  game.updatePageData();
