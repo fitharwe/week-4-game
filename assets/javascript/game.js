5219ee03f5ec0aa6985fd8fe740ce4290f900e4b
fitharwe <jesterenwalker@gmail.com>
2018-04-09 20:14:34 -0400

    working js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,30 @@
 var yourScore = document.getElementById('yourScore');
 var loser = document.getElementById('losses');
 var winner = document.getElementById('wins');
-yourScore.innerHTML = 0; //
 var targetNumber = document.getElementById('targetNumber');
-targetNumber.innerHTML = getRandomValue(10, 4);
+targetNumber.innerHTML = getRandomValue(20, 5);
 
+//function created to add to score, taking into account current score
 function addToScore(val) {
   var numberToAdd = parseInt(val);
   var currentScore = parseInt(yourScore.innerHTML);
   yourScore.innerHTML = numberToAdd + currentScore;
 }
 
+//assigning id's so the html doc can access this data. assigning random values to each gem
 var gem2 = document.getElementById("gem2");
-//gem2.setAttribtarute("value", "5");
-console.log(gem2.value);
+gem2.value = getRandomValue(10, 4);
 
 var gem1 = document.getElementById("gem1");
-
+gem1.value = getRandomValue(10, 4);
 
 var gem3 = document.getElementById("gem3");
-
+gem3.value = getRandomValue(10, 4);
 
 var gem4 = document.getElementById("gem4");
+gem4.value = getRandomValue(10, 4);
 
+// "listens" to the event/click and keeps track of the values selected. make the thumbnail active, not static
 gem2.addEventListener('click', function() {
   addToScore(this.value);
   checkPlayerScore();
@@ -43,6 +45,7 @@ gem4.addEventListener('click', function() {
   checkPlayerScore();
 });
 
+//making getrandomvalue a function that gets a random value with the below equations
 function getRandomValue (a, b){
   return Math.floor((Math.random() * a) + b);
 }
@@ -51,24 +54,18 @@ function checkPlayerScore() {
   var playerInt = parseInt(yourScore.innerHTML);
   var targetInt = parseInt(targetNumber.innerHTML);
   if (playerInt === targetInt) {
-      alert('You win!');
+    winner.innerHTML = parseInt(winner.innerHTML) + 1;
+    reset();
   } else if (playerInt > targetInt) {
-    alert('You lose!');
+    loser.innerHTML = parseInt(loser.innerHTML) + 1;
+    reset();
   } 
 }
 
-console.log(getRandomValue(10, 4));
-
-tempString = loser + "";
-for (var i = tempString.length; i < 43; i++) {
-  tempString += " ";
+//function to reset the score and target number
+function reset() {
+  yourScore.innerHTML = 0;
+  targetNumber.innerHTML = getRandomValue (20, 5);
 }
-totalLoser.textContent = "Losses " + tempString;
 
-tempString = winner + "";
-	for (var i = tempString.length; i < 45; i++) {
-		tempString += " ";
-	}
-  totalWinner.textContent = "Wins " + tempString;
   
-  game.updatePageData();
