94ef16c853c361dec41a99857afdd7855ce51421
fitharwe <jesterenwalker@gmail.com>
2018-04-09 21:35:40 -0400

    making it jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,65 @@
-var yourScore = document.getElementById('yourScore');
-var loser = document.getElementById('losses');
-var winner = document.getElementById('wins');
-var targetNumber = document.getElementById('targetNumber');
-targetNumber.innerHTML = getRandomValue(20, 5);
 
 //function created to add to score, taking into account current score
 function addToScore(val) {
   var numberToAdd = parseInt(val);
-  var currentScore = parseInt(yourScore.innerHTML);
-  yourScore.innerHTML = numberToAdd + currentScore;
+  var currentScore = parseInt($("#yourScore").text());
+  $("#yourScore").text(numberToAdd + currentScore);
 }
 
-//assigning id's so the html doc can access this data. assigning random values to each gem
-var gem2 = document.getElementById("gem2");
-gem2.value = getRandomValue(10, 4);
-
-var gem1 = document.getElementById("gem1");
-gem1.value = getRandomValue(10, 4);
-
-var gem3 = document.getElementById("gem3");
-gem3.value = getRandomValue(10, 4);
-
-var gem4 = document.getElementById("gem4");
-gem4.value = getRandomValue(10, 4);
-
-// "listens" to the event/click and keeps track of the values selected. make the thumbnail active, not static
-gem2.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-gem1.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-gem3.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-gem4.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
 //making getrandomvalue a function that gets a random value with the below equations
-function getRandomValue (a, b){
-  return Math.floor((Math.random() * a) + b);
+function getRandomValue(a, b) {
+  return Math.floor(Math.random() * a + b);
 }
 
 function checkPlayerScore() {
-  var playerInt = parseInt(yourScore.innerHTML);
-  var targetInt = parseInt(targetNumber.innerHTML);
+  var playerInt = parseInt($("#yourScore").text());
+  var targetInt = parseInt($("#targetNumber").text());
   if (playerInt === targetInt) {
-    winner.innerHTML = parseInt(winner.innerHTML) + 1;
+    $("#wins").text(parseInt($("#wins").text()) + 1);
     reset();
   } else if (playerInt > targetInt) {
-    loser.innerHTML = parseInt(loser.innerHTML) + 1;
+    $("#losses").text(parseInt($("#losses").text()) + 1);
     reset();
-  } 
+  }
 }
 
 //function to reset the score and target number
 function reset() {
-  yourScore.innerHTML = 0;
-  targetNumber.innerHTML = getRandomValue (20, 5);
+  $("#yourScore").text("0");
+  $("#targetNumber").text(getRandomValue(120, 19));
 }
 
+$(function() {
+  //start the game by setting the target to random number
+  $("#targetNumber").text(getRandomValue(120, 19));
+
+  // //assigning id's so the html doc can access this data. assigning random values to each gem
+  $("#gem2").data("crystalvalue", getRandomValue(12, 1));
+
+  $("#gem1").data("crystalvalue", getRandomValue(12, 1));
+
+  $("#gem3").data("crystalvalue", getRandomValue(12, 1));
+
+  $("#gem4").data("crystalvalue", getRandomValue(12, 1));
+
+  // "listens" to the event/click and keeps track of the values selected. make the thumbnail active, not static
+  $("#gem2").on("click", function() {
+    addToScore($(this).data("crystalvalue"));
+    checkPlayerScore();
+  });
+
+  $("#gem3").on("click", function() {
+    addToScore($(this).data("crystalvalue"));
+    checkPlayerScore();
+  });
+
+  $("#gem1").on("click", function() {
+    addToScore($(this).data("crystalvalue"));
+    checkPlayerScore();
+  });
+
+  $("#gem4").on("click", function() {
+    addToScore($(this).data("crystalvalue"));
+    checkPlayerScore();
+  });
+});
